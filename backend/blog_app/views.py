24f64845c294918d1ac06b04ea3a62f49eb12117
gabriel.ospina <gabriel.ospina@smartquick.com>
2024-07-30 00:24:53 -0500

    add serializers and views in django

--- backend/blog_app/views.py
+++ backend/blog_app/views.py
@@ -1,3 +1,28 @@
+from rest_framework import generics
 from django.shortcuts import render
+from models import User, Post, Comment
+from serializers import UserSerializer, PostSerializer, CommentSerializer
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class =  UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
